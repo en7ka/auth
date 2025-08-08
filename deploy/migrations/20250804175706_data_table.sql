2f8cb92e0e4c108100a503346e18f8afe0bd0362
Damei Enik <enikdamei55gmail.com>
2025-08-08 16:58:40 -0700

    fix name of database

--- deploy/migrations/20250804175706_data_table.sql
+++ deploy/migrations/20250804175706_data_table.sql
@@ -1,10 +1,11 @@
 -- +goose Up
-create table data (
+create table users (
       id SERIAL PRIMARY KEY,
       username VARCHAR(100) NOT NULL UNIQUE,
       email VARCHAR(100) NOT NULL UNIQUE,
+      role VARCHAR(20) not null,
       created_at timestamp not null default now()
 );
 
 -- +goose Down
-drop table data;
+drop table users;
