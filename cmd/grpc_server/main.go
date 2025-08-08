7675801af018e5bf7398d3d0402b7509676b9cef
Damei Enik <enikdamei55gmail.com>
2025-08-08 00:22:08 -0700

    реализовал

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,1 +1,78 @@
-package grpc_server
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/en7ka/auth/pkg/auth_v1"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedAuthV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Note id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Note: &desc.UserGet{
+			Id: req.GetId(),
+			Info: &desc.UserInfo{
+				Name:  gofakeit.Name(),
+				Email: gofakeit.Email(),
+			},
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateRequest, error) {
+	log.Printf("Note id: %d", req.GetId())
+
+	return &desc.CreateRequest{
+		Note: &desc.UserGet{
+			Id:        req.GetId(),
+			Info:      req.GetUserInfo(),
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*desc.UpdateRequest, error) {
+	log.Printf("Updated note with id: %d", req.GetId())
+
+	return &desc.UpdateResponse{Id: req.GetId()}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*desc.DeleteRequest, error) {
+	log.Printf("Deleted note with id: %d", req.GetId())
+	return &desc.DeleteResponse{Id: req.GetId()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
