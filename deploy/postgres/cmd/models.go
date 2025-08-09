5854b0c3862497138c7c517ba5ef768811f7d56a
Damei Enik <enikdamei55gmail.com>
2025-08-08 20:22:45 -0700

    реализовал сохранение в бд

--- deploy/postgres/cmd/models.go
+++ deploy/postgres/cmd/models.go
@@ -1,1 +1,32 @@
 package cmd
+
+import (
+	"time"
+
+	auth "github.com/en7ka/auth/pkg/user_v1"
+)
+
+type (
+	User struct {
+		ID        int64
+		Username  string
+		Email     string
+		Password  string // Добавлено
+		Role      auth.Role
+		CreatedAt time.Time
+		UpdatedAt time.Time // Добавлено
+	}
+
+	UpdateUser struct {
+		ID       int64
+		Username string // Исправлено с Name на Username
+		Email    string
+	}
+
+	DeleteID int64
+
+	GetUserPar struct {
+		ID       *int64
+		Username *string
+	}
+)
--- deploy/postgres/cmd/query.go
+++ deploy/postgres/cmd/query.go
@@ -1,1 +1,112 @@
 package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
+)
+
+const (
+	dbDSN = "host=localhost port=5433 dbname=users user=data-user password=note-password sslmode=disable"
+)
+
+type Storage struct {
+	con *pgx.Conn
+	ctx context.Context
+}
+
+func InitStorage() (*Storage, error) {
+	ctx := context.Background()
+	dbDSN := getDSN()
+	conn, err := pgx.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatal("Error connecting to database")
+	}
+	return &Storage{con: conn, ctx: ctx}, nil
+}
+
+func getDSN() string {
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		dsn = dbDSN
+	}
+	return dsn
+}
+
+func (s *Storage) CloseCon() {
+	err := s.con.Close(s.ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// PostgresInterface
+type PostgresInterface interface {
+	Save(user User) (int64, error)
+	Update(update UpdateUser) error
+	Delete(id DeleteID) error
+	GetUser(params GetUserPar) (*User, error)
+}
+
+// Save
+func (s *Storage) Save(user User) (int64, error) {
+	var id int64
+	// Возвращаем ID после вставки
+	err := s.con.QueryRow(s.ctx, "INSERT INTO users (username, email, password, role) VALUES ($1, $2, $3, $4) RETURNING id", user.Username, user.Email, user.Password, user.Role).Scan(&id)
+	if err != nil {
+		log.Printf("Error inserting user into database: %v", err)
+		return 0, err
+	}
+	log.Printf("Inserted user into database with id: %d", id)
+	return id, nil
+}
+
+// Update
+func (s *Storage) Update(update UpdateUser) error {
+	res, err := s.con.Exec(s.ctx, "UPDATE users SET username = $1, email = $2 WHERE id = $3", update.Username, update.Email, update.ID)
+	if err != nil {
+		log.Printf("Error updating user into database: %v", err)
+	}
+	log.Printf("Updated user into database: %d", res.RowsAffected())
+	return nil
+}
+
+// Delete
+func (s *Storage) Delete(id DeleteID) error {
+	res, err := s.con.Exec(s.ctx, "DELETE FROM users WHERE id = $1", id)
+	if err != nil {
+		log.Printf("Error deleting user into database: %v", err)
+	}
+	log.Printf("Deleted user into database: %d", res.RowsAffected())
+	return nil
+}
+
+// GetUser
+func (s *Storage) GetUser(params GetUserPar) (*User, error) {
+	var user User
+	var err error
+
+	query := sq.Select("id", "username", "email", "role", "created_at", "updated_at").From("users")
+	switch {
+	case params.ID != nil:
+		query = query.Where(sq.Eq{"id": *params.ID})
+	case params.Username != nil:
+		query = query.Where(sq.Eq{"username": *params.Username})
+	default:
+		return nil, fmt.Errorf("no users and no username")
+	}
+	dbQuery, args, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+	row := s.con.QueryRow(s.ctx, dbQuery, args...)
+	err = row.Scan(&user.ID, &user.Username, &user.Email, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
