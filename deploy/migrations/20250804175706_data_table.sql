67e842457857b59201e1d9b635f8b724cd6a9ff9
Damei Enik <enikdamei55gmail.com>
2025-08-09 12:44:00 -0700

    some fix

--- deploy/migrations/20250804175706_data_table.sql
+++ deploy/migrations/20250804175706_data_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
            id SERIAL PRIMARY KEY,
            username VARCHAR(255) UNIQUE NOT NULL,
            email VARCHAR(255) UNIQUE NOT NULL,
-           password TEXT NOT NULL,
+           password VARCHAR(255) NOT NULL,
            role VARCHAR(50) NOT NULL,
            created_at TIMESTAMPTZ DEFAULT now(),
            updated_at TIMESTAMPTZ DEFAULT now()
