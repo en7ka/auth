d75b71dedd949446b2de3b2c3b06beea182194ed
Damei Enik <enikdamei55gmail.com>
2025-08-15 02:09:11 -0700

    реализовал api слой

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -2,123 +2,60 @@ package main
 
 import (
 	"context"
-	"log"
-	"net"
-
+	"github.com/en7ka/auth/internal/config"
+	desc "github.com/en7ka/auth/pkg/user_v1"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/protobuf/types/known/emptypb"
-	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
 
-	desc "github.com/en7ka/auth/pkg/user_v1"
-
-	"github.com/en7ka/auth/internal/config"
-	"github.com/en7ka/auth/internal/repository/auth"
-	"github.com/en7ka/auth/internal/repository/auth/converter"
-	"github.com/en7ka/auth/internal/repository/auth/model"
-	repinf "github.com/en7ka/auth/internal/repository/repositoryinterface"
+	userApi "github.com/en7ka/auth/internal/api/auth"
+	userRepo "github.com/en7ka/auth/internal/repository/auth"
+	userService "github.com/en7ka/auth/internal/service/auth"
 )
-
-type server struct {
-	desc.UnimplementedUserAPIServer
-	userRepository repinf.UserRepository
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	id, err := s.userRepository.Create(ctx, &model.UserInfo{
-		Username: req.GetInfo().GetUsername(),
-		Email:    req.GetInfo().GetEmail(),
-		Password: req.GetInfo().GetPassword(),
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &desc.CreateResponse{Id: id}, nil
-}
-
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	u, err := s.userRepository.Get(ctx, req.GetId())
-	if err != nil {
-		return nil, err
-	}
-	return &desc.GetResponse{
-		Note: &desc.Note{
-			Id: u.Id,
-			Info: &desc.NoteInfo{
-				Username: u.Info.Username,
-				Email:    u.Info.Email,
-				Password: u.Info.Password,
-				Role:     converter.RoleFromString(u.Role),
-			},
-			CreatedAt: timestamppb.New(u.CreatedAt),
-			UpdatedAt: timestamppb.New(u.UpdatedAt),
-		},
-	}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	id := req.GetId()
-
-	info := &model.UserInfo{}
-	if v := req.GetInfo().GetUsername(); v != nil {
-		info.Username = v.GetValue()
-	}
-	if v := req.GetInfo().GetEmail(); v != nil {
-		info.Email = v.GetValue()
-	}
-
-	if err := s.userRepository.Update(ctx, id, info); err != nil {
-		return nil, err
-	}
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	if err := s.userRepository.Delete(ctx, req.GetId()); err != nil {
-		return nil, err
-	}
-	return &emptypb.Empty{}, nil
-}
 
 func main() {
 	ctx := context.Background()
 
-	if err := config.Load(".env"); err != nil {
-		log.Fatal(err)
+	//считываем переменные окружения
+	err := config.Load(".env")
+	if err != nil {
+		log.Fatalf("ошибка к подключению к .env: %v", err)
 	}
 
-	grpcCfg, err := config.NewGRPCConfig()
+	grpcConfig, err := config.NewGRPCConfig()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("ошибка к подключению с grpc config: %v", err)
 	}
 
-	pgCfg, err := config.NewPGConfig()
+	pgConfig, err := config.NewPGConfig()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("ошибка к подключению к pg config: %v", err)
 	}
 
-	lis, err := net.Listen("tcp", grpcCfg.Address())
+	lis, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("ошибка в прослушивании: %v", err)
 	}
 
-	pool, err := pgxpool.New(ctx, pgCfg.DSN())
+	pool, err := pgxpool.New(ctx, pgConfig.DSN())
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("ошибка в подключении: %v", err)
 	}
 	defer pool.Close()
 
-	repo := auth.NewRepository(pool)
+	noteRepo := userRepo.NewRepository(pool)
+	noteService := userService.NewService(noteRepo)
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterUserAPIServer(s, &server{userRepository: repo})
+	desc.RegisterUserAPIServer(s, userApi.NewImplementation(noteService))
 
-	log.Printf("grpc: %s", grpcCfg.Address())
+	log.Printf("сервер слушает на порту: %v", grpcConfig.Address())
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatal(err)
+	if err = s.Serve(lis); err != nil {
+		log.Printf("ошибка в прослушивании: %v", err)
 	}
+
 }
